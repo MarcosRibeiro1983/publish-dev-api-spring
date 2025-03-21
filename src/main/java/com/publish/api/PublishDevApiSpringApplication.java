44a3290278f6e9d9bbf6b43347b9e23d8f7c7e75
Marcos <marcos@softcontext.com>
2025-03-21 16:08:50 -0300

    connected with neo cloud-database

--- src/main/java/com/publish/api/PublishDevApiSpringApplication.java
+++ src/main/java/com/publish/api/PublishDevApiSpringApplication.java
@@ -2,10 +2,6 @@ package com.publish.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PublishDevApiSpringApplication {
@@ -13,18 +9,6 @@ public class PublishDevApiSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PublishDevApiSpringApplication.class, args);
 	}
-
-
-//	@Bean
-//	public DataSource dataSource(){
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName("org.postgresql.Driver");
-//		dataSource.setUrl("jdbc:postgresql://postgres.railway.internal:5432/railway");
-//		dataSource.setUsername("postgres");
-//		dataSource.setPassword("rHcfFaRUCTamnkghGtGTplhVlTZzsaUb");
-//		return dataSource;
-//	}
-
 
 }
 
